df2317395efa363f7ffc1dd5cd06a46fae0574d5
minhluquang <luminh2004@gmail.com>
2023-12-17 13:01:37 +0700

    14. useSelector, useDispatch (react-redux)

--- src/App.js
+++ src/App.js
@@ -5,30 +5,48 @@ import { connect } from "react-redux";
 
 import { increaseCounter, decreaseCounter } from "./action/actions";
 
+import { useSelector, useDispatch } from "react-redux";
+
 function App(props) {
+  const dispatch = useDispatch();
+
+  const newCount = useSelector((state) => {
+    return state.counter.count;
+  });
+
+  const handleInscrease = () => {
+    dispatch(increaseCounter());
+  };
+
+  const handleDescrease = () => {
+    dispatch(decreaseCounter());
+  };
+
   return (
     <div className="App">
-      <div>Count: {props.count}</div>
+      <div>Count: {newCount}</div>
 
-      <button onClick={() => props.increaseCounter()}>Increase Count</button>
+      <button onClick={() => handleInscrease()}>Increase Count</button>
 
-      <button onClick={() => props.decreaseCounter()}>Decrease Count</button>
+      <button onClick={() => handleDescrease()}>Decrease Count</button>
     </div>
   );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    count: state.counter.count,
-  };
-};
+// const mapStateToProps = (state) => {
+//   return {
+//     count: state.counter.count,
+//   };
+// };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    increaseCounter: () => dispatch(increaseCounter()),
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increaseCounter: () => dispatch(increaseCounter()),
 
-    decreaseCounter: () => dispatch(decreaseCounter()),
-  };
-};
+//     decreaseCounter: () => dispatch(decreaseCounter()),
+//   };
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+export default App;
--- src/action/actions.js
+++ src/action/actions.js
@@ -3,6 +3,7 @@ import { INCREMENT, DECREMENT } from "./types.js";
 export const increaseCounter = () => {
   return {
     type: INCREMENT,
+    payload: { message: "MINH TEST" },
   };
 };
 
